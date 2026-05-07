ad57ac8dbef04c08d1028e8ab6601153935c0ae6
jayedHoshen <jayedkn64@gmail.com>
2026-05-07 12:25:50 +0600

    added error page

--- src/error/ErrorPage.jsx
+++ src/error/ErrorPage.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-gray-900 via-gray-800 to-black text-white px-4">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-[#244D3F] via-[#1B3A30] to-[#10241D] text-white px-4">
       <div className="text-center max-w-lg">
         {/* Error Code */}
-        <h1 className="text-8xl font-extrabold bg-linear-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text">
+        <h1 className="text-8xl font-extrabold bg-linear-to-r from-[#4E9F83] to-[#A7D7C5] text-transparent bg-clip-text">
           404
         </h1>
 
@@ -15,13 +15,13 @@ const ErrorPage = () => {
         </h2>
 
         {/* Description */}
-        <p className="text-gray-400 mt-3">
+        <p className="text-gray-300 mt-3">
           The page you're looking for doesn't exist or has been moved.
         </p>
 
         {/* Button */}
         <Link to="/">
-          <button className="mt-6 px-6 py-3 bg-linear-to-r from-purple-500 to-pink-500 rounded-xl font-medium hover:scale-105 transition duration-300 shadow-lg">
+          <button className="mt-6 px-6 py-3 bg-linear-to-r from-[#2E6B57] to-[#4E9F83] rounded-xl font-medium hover:scale-105 transition duration-300 shadow-lg hover:shadow-[#4E9F83]/40">
             Go Back Home
           </button>
         </Link>
